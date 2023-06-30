67aa388b262ffdda5af34f3eaff5eea3f8c76530
vac765 <valdeva_crema@harvard.edu>
2023-06-29 20:09:39 -0400

    Feature Flag implementation

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -1,6 +1,7 @@
 from celery import Celery
 import os
 import requests
+import json
 
 app = Celery()
 app.config_from_object('celeryconfig')
@@ -15,16 +16,29 @@ def invoke_dims(message):
     # with a healtcheck endpoint for 'hello world'
     r = requests.get(dims_ingest_url, verify=False)
     print(r.text)
-    invoke_hello_world()
+    json_message = json.loads(message)
+    if "feature_flags" in json_message:
+        feature_flags = json_message["feature_flags"]
+        if "send_to_drs_feature_flag" in feature_flags and \
+                feature_flags["send_to_drs_feature_flag"] == "on":
+            # Send to DRS
+            print("FEATURE IS ON>>>>>SEND TO DRS")
+        else:
+            # Feature is off so do hello world
+            invoke_hello_world(json_message)
 
 
 # To be removed when real logic takes its place
-def invoke_hello_world():
+def invoke_hello_world(json_message):
 
     # For 'hello world', we are also going to place a
     # message onto the etd_ingested_into_drs queue
     # to allow the pipeline to continue
     new_message = {"hello": "from etd-alma-monitor-service"}
+    if "feature_flags" in json_message:
+        print("FEATURE FLAGS FOUND")
+        print(json_message['feature_flags'])
+        new_message['feature_flags'] = json_message['feature_flags']
     app.send_task("etd-alma-drs-holding-service.tasks.add_holdings",
                   args=[new_message], kwargs={},
-                  queue="etd_ingested_into_drs")
+                  queue="etd_ingested_into_drs_dd")
