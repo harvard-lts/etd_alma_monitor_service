3e047aa69098093052b756f2a8f566f8323c79eb
Chip Goines <chip_goines@harvard.edu>
2023-07-27 18:46:04 -0400

    Added healthchecks for alma and dims

--- scripts/healthcheck.py
+++ scripts/healthcheck.py
@@ -7,7 +7,33 @@ from pathlib import Path
 hbeat_path = os.getenv("HEARTBEAT_FILE", "/tmp/worker_heartbeat")
 HEARTBEAT_FILE = Path(hbeat_path)
 HEARTBEAT_WINDOW = int(os.getenv("HEARTBEAT_WINDOW", 60))
+ALMA_API_KEY = os.getenv("ALMA_API_KEY")
+ALMA_HEALTHCHECK_URL = os.getenv("ALMA_HEALTHCHECK_URL") + ALMA_API_KEY
+DIMS_HEALTHCHECK_URL = os.getenv("DIMS_HEALTHCHECK_URL")
 
+# check alma
+try:
+    # need verify false b/c using selfsigned certs
+    r = requests.get(ALMA_HEALTHCHECK_URL, verify=False)
+    if (r.status_code != 200):
+        print("alma healthcheck failed")
+        sys.exit(1)
+except Exception:
+    print("alma healthcheck failed")
+    sys.exit(1)
+
+# check dims
+try:
+    # need verify false b/c using selfsigned certs
+    r = requests.get(DIMS_HEALTHCHECK_URL, verify=False)
+    if (r.status_code != 200):
+        print("dims healthcheck failed")
+        sys.exit(1)
+except Exception:
+    print("dims healthcheck failed")
+    sys.exit(1)
+
+# check timestamp file
 try:
     current_ts = int(time.time())
     fstats = os.stat(HEARTBEAT_FILE)
